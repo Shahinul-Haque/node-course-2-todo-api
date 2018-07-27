1a0895ab641b0ddd94a18aab345ea5a3fbe2ac38
Shahinul-Haque <Shahinjava6320@gmail.com>
2018-07-27 19:24:13 +0600

    Test the Delete

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -103,5 +103,41 @@ describe('GET /todos/:id', () => {
     })
 });
 
+describe('DELETE/todos/:id', ()=>{
+     it('shoud be remove a todo',(done)=>{
+        var hexId = todos[1]._id.toHexString();
+          
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo._id).toBe(hexId);
+        }).end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+            Todo.findById(hexId).then((todo)=>{
+                expect(todo).toNotExist();
+                done();
+            }).catch((e)=> done(e));
+        });
+      
+
+     });    
+     it('should be return 404 if todo not found',(done)=>{
+         var hexId = new ObjectID().toHexString();
+          request(app)
+          .delete(`/todos/${hexId}`)
+          .expect(404)
+          .end(done);
+     })
+     it('should return 404 for non object Id',(done)=>{
+        request(app)
+        .delete('/todos/123abc') 
+        .expect(404)
+        .end(done);
+     })
+})
+
 
   
