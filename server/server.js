7e08ff493e26dcb8ea8038d5db30992a1e152c6a
Shahinul-Haque <Shahinjava6320@gmail.com>
2018-08-03 01:55:18 +0600

    Add POST/users/login route

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const {ObjectID} = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var {Todo} = require('./model/todo');
 var {User} = require('./model/user');
-var {authenticate} = require('./middleware/authenticate')
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 app.use(bodyParser.json());
@@ -117,6 +117,18 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login',(req,res)=>{
+  var body = _.pick(req.body,['email','password']);
+  
+  User.findByCredentials(body.email,body.password).then((user)=>{
+     return user.generateAuthToken().then((token)=>{
+        res.header('x-auth',token).send(user);
+     })
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+ 
+})
 
 app.listen(port,()=>{
   console.log(`Started up at  port ${port}`);
